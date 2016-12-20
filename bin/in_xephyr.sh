5fbdf2bf5ded92348e6ab6c8b6993b8375aed143
Vasya Novikov <n1dr+cm3513git@ya.ru>
2016-12-21 00:23:50 +0300

    add some documentation for in_xephyr.sh

--- bin/in_xephyr.sh
+++ bin/in_xephyr.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -eu
 
+# The intention of the script is to source it,
+# in order to make `in_xephyr` bash function available in an external program.
+#
+# You can use this bash function like so:
+#   in_xephyr xterm
+# and it will launch `xterm` inside a newly created X11 (Xephyr) instance.
+#
+# You can also create a multi-line bash function to be executed inside the Xephyr instance,
+# and executi via:
+#   in_xephyr my_bash_func
+
 function in_xephyr() {
 	source xepsource.sh
 	# TODO: set +e; execute the command; capture exit code; set -e; print exit code
