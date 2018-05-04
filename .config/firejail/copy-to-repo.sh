7027d73510eae40243ef19f84b124dee962bb513
Vasya Novikov <n1dr+cm3513git@ya.ru>
2018-05-04 11:32:11 +0300

    update seccomp lists

--- .config/firejail/copy-to-repo.sh
+++ .config/firejail/copy-to-repo.sh
@@ -3,6 +3,4 @@
 # There are reasons why I don't want to put
 # my whole ~/.config/firejail directory in "dotfiles" repo.
 
-target="$PWD"
-cd ~/.config/firejail
-cp -afl ./*-seccomp.inc "$target"
+cp -afl ~/.config/firejail/*-seccomp.inc ./
